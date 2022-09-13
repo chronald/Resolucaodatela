fc9cef3aa81b7f14cc770ca3203cefeb97b0f820
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-09-13 15:37:47 -0300

    atualizando

--- src/ResolucaoDaTela.java
+++ src/ResolucaoDaTela.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class ResolucaoDaTela {
+import java.awt.*;
+
+public class ResolucaoDaTela {
+    public static void main(String[] args) {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("Sua tela tem a resolução:");
+        System.out.println(d.width +"x"+d.height );
+    }
 }
